c8cf17566e37760019cdb66e8d377ecae76e03dc
puskottampandey <puskottam.pandey@gmail.com>
2023-07-08 10:56:07 +0545

    List of product added

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,8 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
+import 'package:flutter/material.dart';
+import 'package:onlinestore/screens/models.dart';
+import 'package:http/http.dart' as http;
 import '../constant/constant.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -10,6 +13,25 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<ProductModel>? productmodel = [];
+  Future<List<ProductModel>>? futureproduct;
+  Future<List<ProductModel>> fetchData() async {
+    final response =
+        await http.get(Uri.parse('https://fakestoreapi.com/products?limit=10'));
+    if (response.statusCode == 200) {
+      List<dynamic> data = (jsonDecode(response.body.toString()));
+      return data.map((json) => ProductModel.fromJson(json)).toList();
+    } else {
+      throw Exception('Failed to load the Prouct');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    futureproduct = fetchData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -54,11 +76,88 @@ class _HomeScreenState extends State<HomeScreen> {
                             hintStyle: TextStyle(color: kcolor)),
                       ),
                     ),
-                  )
+                  ),
+                  const SizedBox()
                 ],
               ),
             ),
-          )
+          ),
+          const Text(
+            "ALL PRODUCT",
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          FutureBuilder<List<ProductModel>>(
+              future: futureproduct,
+              builder: (context, AsyncSnapshot<List<ProductModel>> snapshot) {
+                if (snapshot.hasData) {
+                  List<ProductModel> products = snapshot.data!;
+                  return Expanded(
+                      child: GridView.builder(
+                          padding: const EdgeInsets.all(16.0),
+                          scrollDirection: Axis.vertical,
+                          itemCount: products.length,
+                          gridDelegate:
+                              const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2,
+                            crossAxisSpacing: 8,
+                            mainAxisSpacing: 8,
+                          ),
+                          itemBuilder: (context, index) {
+                            final product = products[index];
+                            return Container(
+                              width: 180,
+                              decoration: const BoxDecoration(
+                                color: Colors.white,
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(8)),
+                              ),
+                              child: Column(
+                                children: [
+                                  Container(
+                                    height: 120,
+                                    width: 200,
+                                    decoration: const BoxDecoration(
+                                      color: Colors.white,
+                                      borderRadius:
+                                          BorderRadius.all(Radius.circular(8)),
+                                    ),
+                                    child: ClipRRect(
+                                      borderRadius:
+                                          BorderRadiusDirectional.circular(8),
+                                      child: Image.network(
+                                          product.image.toString(),
+                                          fit: BoxFit.contain),
+                                    ),
+                                  ),
+                                  /*
+                                  Image.network(
+                                    product.image.toString(),
+                                    fit: BoxFit.cover,
+                                  ),
+                                  */
+                                  Text(
+                                    product.title.toString(),
+                                    style: const TextStyle(
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  Text(product.price.toString()),
+                                ],
+                              ),
+                            );
+                          })
+
+                      /* ListView.builder(
+                        itemCount: products.length,
+                        itemBuilder: (context, index) {
+                          final product = products[index];
+                          return Text(product.title.toString());
+                        }), */
+                      );
+                } else if (snapshot.hasError) {
+                  return Text('${snapshot.error}');
+                }
+                return const CircularProgressIndicator();
+              })
         ],
       ),
     );
---
+++ lib/screens/models.dart
@@ -0,0 +1,46 @@
+class ProductModel {
+  int? id;
+  String? title;
+  double? price;
+  String? description;
+  String? category;
+  String? image;
+  /*
+  Rating? rating;
+*/
+  ProductModel({
+    this.id,
+    this.title,
+    this.price,
+    this.description,
+    this.category,
+    this.image,
+    /*this.rating */
+  });
+
+  factory ProductModel.fromJson(Map<String, dynamic> json) {
+    return ProductModel(
+      id: json['id'],
+      title: json['title'],
+      /* price: json['price'], */
+      description: json['description'],
+      category: json['category'],
+      image: json['image'],
+      /* rating: json['rating'] != null ? Rating.fromJson(json['rating']) : null,  */
+    );
+  }
+}
+
+class Rating {
+  double? rate;
+  int? count;
+
+  Rating({this.rate, this.count});
+
+  factory Rating.fromJson(Map<String, dynamic> json) {
+    return Rating(
+      rate: json['rate'],
+      count: json['count'],
+    );
+  }
+}
